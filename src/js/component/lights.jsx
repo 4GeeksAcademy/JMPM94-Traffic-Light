b9437cc5bd1524c1a851034ca80ad4bd9d779fd8
JMPM94 <137455695+JMPM94@users.noreply.github.com>
2023-11-03 19:52:29 +0000

    First commit

---
+++ src/js/component/lights.jsx
@@ -0,0 +1,15 @@
+import react, { useState } from "react";
+
+const lights = () => {
+    const [colorRedLight, setcolorRedLight] = useState("Red")
+    function redLight () {
+        setcolorRedLight("Red")
+    };
+    if (setcolorRedLight = ("Red"))
+    return (<>
+    <button>Red</button>
+		<button>Yellow</button>
+		<button>Red</button>
+    </>
+    );
+};
